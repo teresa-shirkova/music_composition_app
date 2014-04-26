5820aec239d3b051c5b22b84963694b21601157e
Teresa Murphy-Skvorzova <tesmurphy@gmail.com>
2014-04-26 16:49:48 -0400

    almost to carousels

---
+++ js/stave_notes.js
@@ -0,0 +1,10 @@
+
+var make_stave = function () {
+  var canvas = $("div.one div.a canvas")[0];
+  var renderer = new Vex.Flow.Renderer(canvas,
+    Vex.Flow.Renderer.Backends.CANVAS);
+
+  var ctx = renderer.getContext();
+  var stave = new Vex.Flow.Stave(10, 0, 500);
+  stave.addClef("treble").setContext(ctx).draw();
+};
