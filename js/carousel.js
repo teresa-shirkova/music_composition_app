1c3a072c8356b2198c41baab5cb3f2e52c15ff8e
bitdivine <bitdivine@dmdt.org>
2014-04-27 04:21:08 +0000

    Save notes

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,11 @@
 
 
 $(document).ready(function() {
+	var music = [];
 	harnessCarousel(function(tuple){
+		music.push("abcdefgh".substr(tuple[2],1));
+		var notes = music.map(function(letter){return new Vex.Flow.StaveNote({ keys: [letter+"/4"], duration: "q" });});
+		// write_notes(notes);
 		write_s_note("abcdefgh".substr(tuple[2],1));
 	});
 });
