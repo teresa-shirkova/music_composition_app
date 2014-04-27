ca2eb5119006e0af37fc4ad60cf5016d8b514541
bitdivine <bitdivine@dmdt.org>
2014-04-27 03:25:04 +0000

    Carousel - revive

--- js/carousel.js
+++ js/carousel.js
@@ -1,26 +1,7 @@
 
 
-var write_play_off = function() {
-    $('#write_play_carousel').carouFredSel({auto:true}).trigger('pause', true);
-};
-
-
-var note_type_off = function() {
-    $('#note_length_carousel').carouFredSel({auto:true}).trigger('pause', true);
-
-};
-
-var note_name_off = function() {
-    $('#note_name_carousel').carouFredSel({auto:false}).trigger('pause', true);
-};
-
 $(document).ready(function() {
-note_name_off();
-note_type_off();
-write_play_off();
-$('#write_play_carousel').keypress(
-    $('#write_play_carousel').trigger('resume', true)
-);
+	harnessCarousel(alert);
 });
 
 
