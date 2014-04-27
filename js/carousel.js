a795cd2897030c9ce6df39ea1ef1eac9973956e0
bitdivine <bitdivine@dmdt.org>
2014-04-27 12:45:49 +0000

    Addeleting the last note now works

--- js/carousel.js
+++ js/carousel.js
@@ -3,10 +3,20 @@
 $(document).ready(function() {
 	var music = [];
 	harnessCarousel(function(tuple){
-		music.push("abcdefgh".substr(tuple[2],1));
-		var notes = music.map(function(letter){return new Vex.Flow.StaveNote({ keys: [letter+"/4"], duration: "q" });});
-		// write_notes(notes);
-		write_s_note("abcdefgh".substr(tuple[2],1));
+		switch (tuple[0]) {
+			case 0:
+				music.push("abcdefgh".substr(tuple[2],1)+'/'+'4821'.substr(tuple[1],1));
+				var notes = music.map(function(note){return new Vex.Flow.StaveNote({ keys: [note], duration: "q" });});
+				// write_notes(notes);
+				write_s_note("abcdefgh".substr(tuple[2],1));
+				break;
+			case 1:
+				break;
+			case 2:
+				if (music.length>0) music.pop();
+				break;
+		}
+		console.log(music);
 	});
 });
 
