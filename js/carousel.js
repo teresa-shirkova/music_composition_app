6f94ce8003dadea7d37e94ce00e8a0ea56336888
Teresa Murphy-Skvorzova <tesmurphy@gmail.com>
2014-04-26 23:15:43 -0400

    first notes

--- js/carousel.js
+++ js/carousel.js
@@ -14,27 +14,14 @@ var note_name_off = function() {
     $('#note_name_carousel').carouFredSel({auto:false}).trigger('pause', true);
 };
 
-var write_stave = function() {
-  var canvas = $("div.top div.stave canvas")[0];
-  var renderer = new Vex.Flow.Renderer(canvas,
-    Vex.Flow.Renderer.Backends.CANVAS);
-
-  var ctx = renderer.getContext();
-  var stave = new Vex.Flow.Stave(10, 0, 1000);
-  stave.addClef("treble");
-  stave.addTimeSignature("4/4");
-stave.setContext(ctx).draw();
-
-};
-
 
 $(document).ready(function() {
 note_name_off();
 note_type_off();
 write_play_off();
-write_stave();
 $('#write_play_carousel').keypress(
     $('#write_play_carousel').trigger('resume', true)
 );
 });
+
 
--- js/stave_notes.js
+++ js/stave_notes.js
@@ -1,11 +1,94 @@
+var stave = new Vex.Flow.Stave(10, 0, 1000);
 
-var make_stave = function () {
-  var canvas = $("div.one div.a canvas")[0];
-  var renderer = new Vex.Flow.Renderer(canvas,
-    Vex.Flow.Renderer.Backends.CANVAS);
+var write_stave = function() {
+var canvas = $("div.top div.stave canvas")[0];
+var renderer = new Vex.Flow.Renderer(canvas,
+Vex.Flow.Renderer.Backends.CANVAS);
 
-  var ctx = renderer.getContext();
-  var stave = new Vex.Flow.Stave(10, 0, 500);
-  stave.addClef("treble").setContext(ctx).draw();
+var ctx = renderer.getContext();
+  stave.addClef("treble");
   stave.addTimeSignature("4/4");
+stave.setContext(ctx).draw();
+
 };
+
+var write_notes = function() {
+var canvas = $("div.top div.stave canvas")[0];
+var renderer = new Vex.Flow.Renderer(canvas,
+Vex.Flow.Renderer.Backends.CANVAS);
+var ctx = renderer.getContext();
+var notes = [
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
+
+    new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
+
+    new Vex.Flow.StaveNote({ keys: ["f/4"], duration: "q" }),
+
+    new Vex.Flow.StaveNote({ keys: ["e/4"], duration: "q" }),
+];
+
+ var voice = new Vex.Flow.Voice({
+    num_beats: 4,
+    beat_value: 4,
+    resolution: Vex.Flow.RESOLUTION
+  });
+ voice.addTickables(notes);
+
+var formatter = new Vex.Flow.Formatter().
+    joinVoices([voice]).format([voice], 200);
+
+ voice.draw(ctx, stave);
+};
+
+
+var write_s_note = function(entry) {
+var canvas = $("div.top div.stave canvas")[0];
+var renderer = new Vex.Flow.Renderer(canvas,
+Vex.Flow.Renderer.Backends.CANVAS);
+var ctx = renderer.getContext();
+
+var notes = [
+    new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
+];
+
+ var voice = new Vex.Flow.Voice({
+    num_beats: 4,
+    beat_value: 4,
+    resolution: Vex.Flow.RESOLUTION
+  });
+ voice.addTickables(notes);
+
+var formatter = new Vex.Flow.Formatter().
+    joinVoices([voice]).format([voice], 200);
+
+ voice.draw(ctx, stave);
+};
+
+$(document).ready(function() {
+write_stave();
+write_notes();
+write_s_note()
+});
+
+$(document).keydown(function(e){
+        switch (e.keyCode) {
+        case 65:
+               write_s_note(entry);
+
+}
+});
+
+/*
+$(document).keydown(function(e){
+        switch (e.keyCode) {
+        case 65:
+                $('#write_play_carousel').trigger('', true); break;
+        case 66:
+                $('#write_play_carousel').trigger('resume', true); break;
+        case 67:
+                $('#write_play_carousel').trigger('resume', true); break;
+        case 68:
+                $('#write_play_carousel').trigger('resume', true); break;
+}
+});*/
+
